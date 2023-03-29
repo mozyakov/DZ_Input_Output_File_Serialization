baa41d311b7027da8eaa91b4380f15ae54df3d20
mozyakov <s.mozyakov@gmail.com>
2023-03-29 17:12:38 +0700

    подготовка класса баскет

--- src/Basket.java
+++ src/Basket.java
@@ -1,2 +1,15 @@
-public class Basket {
+import java.io.*;
+public class Basket implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String[] goods;
+    private int[] prices;
+    private int[] quantities;
+
+    public Basket() {
+    }
+    public Basket(String[] goods, int[] prices) {
+
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static String[] products = {"Хлеб", "Квас", "Мясо"};
+    static int[] prices = {10, 20, 30};
+
+    public static void main(String[] args) {
+        Basket basket = new Basket(products, prices);
+
+        while(true) {
+            showPrice();
+            System.out.println("Выберите товар через кол-во и пробел или введите 'end'");
+            String input = scanner.nextLine(); //юзер вводит данные
+            if (input.equals("end")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+
+        }
 
 
 
